f5bb6ddd0ad6aa316fc6334ed521a99c6d292be2
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-02 12:15:03 -0500

    Renamed heartbeat to tiny_heartbeat

--- src/tiny_heartbeat.c
+++ src/tiny_heartbeat.c
@@ -4,7 +4,7 @@
  */
 
 #include "pico/stdlib.h"
-#include "heartbeat.h"
+#include "tiny_heartbeat.h"
 
 #ifdef PICO_DEFAULT_LED_PIN
 
@@ -16,13 +16,13 @@ static void blink(tiny_timer_group_t* group, void* context)
   gpio_put(PICO_DEFAULT_LED_PIN, state = !state);
 }
 
-void heartbeat_init(tiny_timer_group_t* timer_group)
+void tiny_heartbeat_init(tiny_timer_group_t* timer_group)
 {
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-  static tiny_timer_t heartbeat_timer;
-  tiny_timer_start_periodic(timer_group, &heartbeat_timer, 500, NULL, blink);
+  static tiny_timer_t tiny_heartbeat_timer;
+  tiny_timer_start_periodic(timer_group, &tiny_heartbeat_timer, 500, NULL, blink);
 }
 
 #endif
