8105c5cf2707d1e147544508ad8d981d06c87c08
Ryan Hartlage <ryanplusplus@gmail.com>
2024-12-07 09:23:22 -0500

    Add blink for Pico W, Pico 2W

--- src/tiny_heartbeat.c
+++ src/tiny_heartbeat.c
@@ -25,3 +25,22 @@ void tiny_heartbeat_init(tiny_timer_group_t* timer_group)
 }
 
 #endif
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+
+#include "pico/cyw43_arch.h"
+
+static void blink(void* context)
+{
+  (void)context;
+  static bool state;
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state = !state);
+}
+
+void tiny_heartbeat_init(tiny_timer_group_t* timer_group)
+{
+  static tiny_timer_t tiny_heartbeat_timer;
+  tiny_timer_start_periodic(timer_group, &tiny_heartbeat_timer, 500, NULL, blink);
+}
+
+#endif
